c3ba24adf419550f79beac1b54d84e2fbe369b30
Aligege <linzx91@users.noreply.github.com>
2019-07-02 08:19:02 +0800

    适配 Surge 4.0

--- Ruleset/Scripts/Zhihu_Answer.js
+++
@@ -1,9 +0,0 @@
-// [MITM]
-// hostname = api.zhihu.com
-// http-response https?://api.zhihu.com/v4/questions script-path=https://github.com/linzx91/surge-ruleset/raw/master/Ruleset/Scripts/Zhihu_Answer.js
-
-var obj = JSON.parse(body);
-
-delete obj['ad_info']
-
-JSON.stringify(obj);
